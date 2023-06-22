8249d84a5223187b55c192717472edc60cf2c604
DANENARIE <136543262+DANENARIE@users.noreply.github.com>
2023-06-22 09:15:03 +0800

    i ada smooth scrolling in my header and also i add a about section and i current working on projects
    home and about container i continue and add more design

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+//Dark Mode effect//
 let darkModeIcon = document.querySelector("#darkMode-icon");
 darkModeIcon.onclick = () => {
   darkModeIcon.classList.toggle("bx-sun");
   document.body.classList.toggle("night-theme");
+};
+
+//header scroll toggle//
+window.onscroll = () => {
+  let header = document.querySelector(".header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
 };
