0a9d249c2184dec829a68c10a2ebed30203b877a
DANENARIE <136543262+DANENARIE@users.noreply.github.com>
2023-07-05 11:27:00 +0800

    update some projects

--- src/main.js
+++ src/main.js
@@ -6,8 +6,58 @@ darkModeIcon.onclick = () => {
 };
 
 //header scroll toggle//
-window.onscroll = () => {
+window.addEventListener("scroll", () => {
   let header = document.querySelector(".header");
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
 
   header.classList.toggle("sticky", window.scrollY > 100);
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+});
+
+//==SCROLL SECTION ACTIVE LINKS====//
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
 };
+
+//menu icon bar //
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+ScrollReveal({
+  reset: true,
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img img, .services-container, .portfolio-box, .contact form",
+  { origin: "buttom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h3, .home-content p,", {
+  origin: "right",
+});
